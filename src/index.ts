e25dd7563ae6332aa73019b7fcdddf556ff25889
ArijKouki <arijkouki17@gmail.com>
2023-05-07 17:02:37 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+import * as fs from 'fs';
+import { Query } from './resolvers/query';
+const typeDefs = fs.readFileSync('./src/schema/schema.graphql', 'utf-8');
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs:typeDefs ,
+    resolvers: {
+      Query: Query
+    }
+  })
+})
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
